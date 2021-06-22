4d7f85b98a21430ca6a2567de930d393fd0801fb
Yayutan <chuangmic25@gmail.com>
2021-06-23 00:57:39 +0800

    Added count for q4, need to write test to print out and check

--- a3.cpp
+++ a3.cpp
@@ -1,27 +1,61 @@
 #include <iostream>
 #include "a3.h"
 using namespace std;
+// TODO function to print a vector
 
-int main()
+void testQ1()
 {
 	int a1[3] = {2, 2, 1};
 	int s1 = 3;
 	int q1 = cartesianProduct(a1, s1);
 	cout << "Q1(n=3): " << to_string(q1) << endl;
+}
 
+void testQ2()
+{
 	int x2 = 5;
 	int q2 = triangle(x2);
 	cout << "Q2(x=5): " << to_string(q2) << endl;
+}
 
+void testQ3()
+{
 	int a3_worse[5] = {1, 2, 3, 4, 5};
 	int s3 = 5;
 	int q3_worse;
-	removeDuplicates(a3_worse, s3, q3_worse);
+	vector<int> v3_worse = removeDuplicates(a3_worse, s3, q3_worse);
 	cout << "Q3_Worse(n=5): " << to_string(q3_worse) << endl;
+	// Check resulting vector
 
 	int a3_best[5] = {1, 1, 1, 1, 1};
 	int q3_best;
-	removeDuplicates(a3_best, s3, q3_best);
+	vector<int> v3_best = removeDuplicates(a3_best, s3, q3_best);
 	cout << "Q3_Best(n=5): " << to_string(q3_best) << endl;
+	// Check resulting vector
+}
+
+void testQ4()
+{
+	int a4[9] = {1,2,3,0,1,0,1,0,1};
+	int r4 = 3;
+}
+
+void testQ5()
+{
+
+}
+
+void testQ6()
+{
+
+}
+
+int main()
+{
+
+
+
+
+
 	return 0;
 }
--- a3.h
+++ a3.h
@@ -77,7 +77,6 @@ vector<int> removeDuplicates(int arr[], int n, int &cnt_op)
 		cnt_op++; // line 75
 		bool duplicate = false;
 		cnt_op++; // line 77
-//		cout << "Before inner loop: " << to_string(cnt_op) << endl;
 		while (cnt_op++, (iResult < (int)result.size() && !duplicate)) { // line 80
 			if ((cnt_op++,arr[i] == result[iResult])) { // line 81
 				duplicate = true;
@@ -87,52 +86,55 @@ vector<int> removeDuplicates(int arr[], int n, int &cnt_op)
 			iResult++;
 			cnt_op++; // line 86
 		}
-//		cout << "After inner loop: " << to_string(cnt_op) << endl;
-		if ((cnt_op++, !duplicate)) { // line 90
+		if ((cnt_op++, !duplicate)) { // line 89
 			result.push_back(arr[i]); //note 4
-			cnt_op++; // line 91
+			cnt_op++; // line 90
 		}
 
 		i++;
-		cnt_op++; // line 95
+		cnt_op++; // line 94
 	}
 	return result;
 }
-//
-////Question 4
-////This function returns an array in dynamic memory that represents the matrix that is the result of multiplying the matrix (array) parameter by itself.
-//// PRE: m represents a square matrix of size rows * rows
-//// PARAM: rows represents the number of both rows and columns
-//// POST: Returns a pointer to an array (matrix) of the same size as m
-//// NOTE: values are indexed r0c0,r0c1,…,r0cn-1,r1c0,…
-//int* matrixSelfMultiply(int* m, int rows)
-//{
-//	// Create result array
-//	int columns = rows;
-//	int* result = new int[rows * columns];
-//	int r = 0;
-//
-//	while (r < rows) {
-//		int c = 0;
-//		while (c < columns) { //columns = rows
-//			int next = 0;
-//			int iNext = 0;
-//
-//			while (iNext < rows) {
-//				next += m[rcIndex(r, iNext, columns)] * m[rcIndex(iNext, c, columns)];
-//				iNext++;
-//			}
-//
-//			result[rcIndex(r, c, columns)] = next;
-//			c++;
-//		}
-//		r++;
-//	}
-//	return result;
-//}
-//// Returns the index of a 1d array representing a matrix
-//// given row (r) and column (c) values
-//int rcIndex(int r, int c, int columns)
-//{
-//	return r * columns + c;
-//}
+
+int* matrixSelfMultiply(int* m, int rows, int &cnt_op)
+{
+	cnt_op = 0;
+	int columns = rows;
+	cnt_op++; // line 103
+	int* result = new int[rows * columns];
+	cnt_op++; // line 105
+	int r = 0;
+	cnt_op++; // line 107
+
+	while ((cnt_op++, r < rows)) { // line 110
+		int c = 0;
+		cnt_op++; // line 111
+		while ((cnt_op++, c < columns)) { //columns = rows  // line 113
+			int next = 0;
+			cnt_op++; // line 114
+			int iNext = 0;
+			cnt_op++; // line 116
+
+			while ((cnt_op++, iNext < rows)) { // line 119
+				next += m[rcIndex(r, iNext, columns)] * m[rcIndex(iNext, c, columns)];
+				cnt_op++; // line 120
+				iNext++;
+				cnt_op++; // line 122
+			}
+
+			result[rcIndex(r, c, columns)] = next;
+			cnt_op++; // line 126
+			c++;
+			cnt_op++; // line 128
+		}
+		r++;
+		cnt_op++; // line 131
+	}
+	return result;
+}
+
+int rcIndex(int r, int c, int columns)
+{
+	return r * columns + c;
+}
